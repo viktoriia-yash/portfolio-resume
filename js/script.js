09e13bca526b4b1cf791b7e0af3f00da73736a75
viktoriia-yash <yashkina.vic@gmail.com>
2022-09-01 16:25:13 +0200

    navbar + about me + contact me pages done

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
+  const menu = document.querySelector(".menu-btn");
+  const navBar = document.querySelector(".navbar-links");
+  const navBarLinks = document.querySelectorAll(".navbar-links li");
 
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
+  menu.addEventListener("click", () => {
+    navBar.classList.toggle("nav-active");
 
-    navLinks.forEach((link, index) => {
+    navBarLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = "";
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 5 + 0.5
+        link.style.animation = `navLinksFade 0.5s ease forwards ${
+          index / 7 + 0.5
         }s`;
       }
     });
-    burger.classList.toggle("toggle");
+    menu.classList.toggle("toggle");
   });
 };
 
